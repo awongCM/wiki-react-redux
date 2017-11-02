2d8f9b7f0feb0d5890b993ceb7f51f0a954cda64
awongCM <awong.cm@gmail.com>
2017-11-02 17:02:36 +1100

    fixed webpack config for image types loading

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+//TODO - not really a good practice for 3rd party UI framework
+import "./../node_modules/materialize-css/dist/js/materialize.js";
+import "./../node_modules/materialize-css/dist/css/materialize.css";
+
 import './index.scss';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
--- webpack.common.js
+++ webpack.common.js
@@ -1,8 +1,7 @@
-const path = require("path")
+const path = require("path");
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-
 
 //For html-webpack-plugin
 const options = {
@@ -22,8 +21,9 @@ module.exports = {
       {test: /\.js$/, loader:'babel-loader', exclude: /node_modules/},
       {test: /\.jsx$/, loader:'babel-loader', exclude: /node_modules/},
       {test: /\.(css|scss)$/, loader:'style-loader!css-loader!sass-loader'},
-      {test: /\.(png|jpg|jpeg|gif|svg)$/, loader:'file-loader'}
-
+      {test: /\.(png|jpg|jpeg|gif)$/, loader:'file-loader'},
+      {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+      {test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader?name=src/[name].[ext]'}
     ]
   },
   plugins: [new HtmlWebpackPlugin(options), new CleanWebpackPlugin(['dist'])]
