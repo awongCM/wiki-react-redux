de9d4d718557f09b043860da6cb99d4116afd657
awongCM <awong.cm@gmail.com>
2017-11-05 14:28:53 +1100

    added Main.scss

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import './Main.scss';
 
 class Main extends Component {
   render() {
     return (
-      <main>
+      <main className="Main">
         <h1>Hello Main Page</h1>
       </main>
     );
