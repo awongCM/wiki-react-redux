6370be28b611c5ae343553aea72c27722bb5f734
awongCM <awong.cm@gmail.com>
2017-11-09 16:03:09 +1100

    modified wikiform layout

--- src/components/WikiForm.js
+++ src/components/WikiForm.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
+import './WikiForm.scss';
 
 class WikiForm extends Component {
   render() {
     return (
       <div className="WikiForm">
         <h2>WikiForm</h2>
-        <form action="">
-          <label htmlFor="">Title</label>
-          <input type="text"/>
-          <label htmlFor="">Content</label>
-          <input type="text"/>
-          <input type="submit"/>
-        </form>
+        <div className="row">
+          <div className="col s12">
+            <form class="form-container" action="">
+              <div className="input-field">
+                <label htmlFor="title" >Title</label>
+                <input type="text" id="title"/>
+              </div>
+              <div className="input-field">
+                <label htmlFor="content">Content</label>
+                <input type="text" id="content" />
+              </div>
+              <div className="input-field">
+                <label htmlFor="author">Author</label>
+                <input type="text" id="author" />
+              </div>
+              <button className="btn waves-effect waves-light" type="submit" name="action">Submit</button>
+            </form>
+          </div>
+        </div>
+        
         <Link to='/wiki'>Return to Wiki List</Link>
       </div>
     );
