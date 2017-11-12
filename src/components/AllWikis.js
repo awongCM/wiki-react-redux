700fecdb27d42d80b143376820c0293093ffb073
awongCM <awong.cm@gmail.com>
2017-11-12 11:21:57 +1100

    refactored the All Wikis UI logic

--- src/components/AllWikis.js
+++ src/components/AllWikis.js
@@ -16,30 +16,43 @@ class AllWikis extends Component {
     return wikiDateFormat.toGMTString();
   }
 
+
+
   render() {
     const {wikis} = this.props;
 
+    let allWikisContent = (
+      <h1>You do not have any wiki content at present</h1>
+    );
+
+    if (wikis.length > 0) {
+      allWikisContent = (
+        <ul>
+        {
+          wikis.map( (wiki, index) => (
+            <li key={index}>
+              <div className="content">
+                <Link to={`/wiki/${index}`}><h4>{wiki.title}</h4></Link>
+                <p>{wiki.content}</p>
+                <small>Author: {wiki.author}</small>
+              </div>
+            </li>
+          ))
+        }
+        </ul>
+      );
+    }
+
     return (
       //TODO - use multiple components per route
+      
       <div className="row">
         <div className="col s3">
           <SideBarContainer></SideBarContainer>
         </div>
         <div className="col s9">
           <div className="AllWikis">
-            <ul>
-            {
-              wikis.map( (wiki, index) => (
-                <li key={index}>
-                  <div className="content">
-                    <Link to={`/wiki/${index}`}><h4>{wiki.title}</h4></Link>
-                    <p>{wiki.content}</p>
-                    <small>Author: {wiki.author}</small>
-                  </div>
-                </li>
-              ))
-            }
-            </ul>
+            {allWikisContent}
           </div>
         </div>
       </div>     
